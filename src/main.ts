8be93cf545c8e06298e2b4678600523259957d7b
Simon Woolf <simon@ably.io>
2018-10-11 17:59:39 +0200

    Add ably

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import * as Ably from 'ably';
 import { platformBrowserDynamic } from 'angular-ts-decorators';
 import { AppModule } from './app/app.module';
 
+const ably = new Ably.Realtime({ key: 'foo.bar:baz' });
 platformBrowserDynamic().bootstrapModule(AppModule);
